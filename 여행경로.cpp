2050d54cebd264bc74de90fae983e83925252106
Yulme <38009216+ImYurim@users.noreply.github.com>
2020-10-17 23:25:00 +0900

    Update 여행경로.cpp

--- 여행경로.cpp
+++ 여행경로.cpp
@@ -8,44 +8,38 @@
 #include <algorithm>
 
 using namespace std;
-queue<string>answer;
+vector<string>answer;
+
 
 
 bool check[10000] = { false };
 
-void dfs(vector<vector<string>>tickets,string start,int count , queue<string>result) {
+void dfs(vector<vector<string>>tickets,string start,int count , vector<string>result) {
 	//모든 배열 체크가 완료된 상태면  빠져나가기
 	if (count == tickets.size()) { 
 		answer = result;
 		return;
 	}
-
+	int same = 0;
 	for (int i = 0; i < tickets.size(); i++) {
 		if (tickets[i][0] == start && check[i]==false) {
+			same++;
+			if (same != 1)continue;
 			check[i] = true;
-			result.push(tickets[i][0]);
+			result.push_back(tickets[i][1]);
 			dfs(tickets, tickets[i][1],count+1,result);
-			result.pop();
+			result.pop_back();
 			check[i] = false;
 		}
 	}
-	return;
 
 }
 
-queue<string> solution(vector<vector<string>> tickets) {
+vector<string> solution(vector<vector<string>> tickets) {
 
 	//알파벳 순으로 정렬
 	sort(tickets.begin(), tickets.end());
-	//출발지가 ICN인 티켓이 여러장일 경우 가장 알파벳순이 빠른 애를 출발로
-	int i;
-	for (i = 0; i < tickets.size(); i++) {
-		if (tickets[i][0] == "ICN") {
-			check[i] = true;
-			break;
-		}
-	}
-	answer.push("ICN");
-	dfs(tickets,tickets[i][1],0,answer);
+	answer.push_back("ICN");
+	dfs(tickets, "ICN", 0, answer);
 	return answer;
 }
