97ee5da09167e8ca043d8a9c38c63544924a44f4
Yulme <38009216+ImYurim@users.noreply.github.com>
2021-01-16 09:19:08 +0900

    Update 소수 찾기.cpp

--- 소수 찾기.cpp
+++ 소수 찾기.cpp
@@ -15,7 +15,8 @@ int main(){
         temp=0;
         
         if(num==1)continue;
-
+        
+        //소수 찾기 할 때, 주어진 수를 2로 나눈 수까지만 나눠지는지 확인해도 됨
         for(i=1; i<=num/2; i++){
             if(num%i==0){
                 temp++;
