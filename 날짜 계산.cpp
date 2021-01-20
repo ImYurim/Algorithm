ab99db47d579e02b034c7b9f603251891fc02b07
Yulme <38009216+ImYurim@users.noreply.github.com>
2021-01-20 17:37:28 +0900

    Update 날짜 계산.cpp

--- 날짜 계산.cpp
+++ 날짜 계산.cpp
@@ -27,3 +27,26 @@ int main(){
     }
     printf("%d",year);
 }
+
+// #include <iostream>
+
+// int year;
+// int E;
+// int e;
+// int S;
+// int s;
+// int M;
+// int m;
+
+// int main(){
+//     scanf("%d %d %d", &E, &S, &M);
+//     year=1;
+//     e=0;
+//     s=0;
+//     m=0;
+//     while(!((year-1)%15+1==E&&(year-1)%28+1==S&&(year-1)%19+1==M)){
+//         year++;
+
+//     }
+//     printf("%d",year);
+// }
