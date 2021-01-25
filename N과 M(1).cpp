00fd2d8f9cc48cb32ccdf1aaec38a367e384be07
Yulme <38009216+ImYurim@users.noreply.github.com>
2021-01-25 18:58:27 +0900

    Create N과 M(1).cpp

---
+++ N과 M(1).cpp
@@ -0,0 +1,38 @@
+//백준 15649번
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int N,M;
+vector<int> arr;
+
+bool visited[9];
+
+void dfs(int cnt){
+    if(cnt==M){
+        for(int i=0; i<arr.size(); i++){
+            printf("%d ",arr[i]);
+        }
+        printf("\n");
+        return;
+    }
+    for(int i=1; i<=N; i++){
+        if(visited[i])continue;
+        visited[i]=true;
+        arr.push_back(i);
+        dfs(cnt+1);
+        visited[i]=false;
+        arr.pop_back();
+    }
+}
+
+int main(){
+    scanf("%d %d",&N,&M);
+    
+    memset(visited, false, sizeof(visited));
+    
+    dfs(0);
+
+}
