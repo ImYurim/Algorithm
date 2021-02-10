1bea50a06cca8c94a1b658b0be5b38b9bba13da9
Yulme <38009216+ImYurim@users.noreply.github.com>
2021-02-10 20:20:25 +0900

    Create 퇴사.cpp

---
+++ 퇴사.cpp
@@ -0,0 +1,16 @@
+//백준 14501번
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int tp[16][2];
+
+int main(){
+    scanf("%d",&N);
+    for(int i=0; i<N; i++){
+        scanf("%d %d",&tp[i][0],&tp[i][1]);
+        
+    }
+    
+}
